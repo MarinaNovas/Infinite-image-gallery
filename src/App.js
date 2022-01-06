593df7ca03052446de9fb7818dd78c25acf1c2b1
Marina Smirnova <marina.smirnova.13.07@gmail.com>
2022-01-06 08:17:25 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useEffect, useState} from 'react';
 import './App.css';
 
+
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery]=useState('');
+
+  function getImages() {
+    let apiUrl = `https://api.unsplash.com/photos/?`;
+    if(query) apiUrl = `https://api.unsplash.com//search/photos/?query=${query}`;
+    apiUrl+=`&page=${page}`;
+    apiUrl+=`&client_id=${accessKey}`;
+    console.log(apiUrl);
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const requestResult = data.results??data;
+        setImages([...images,...requestResult])})
+      .catch((err)=>alert(err));
+  }
+
+  function searchImages(){
+    fetch(`https://api.unsplash.com//search/photos/?page=${page}&query=${query}&client_id=${accessKey}`)
+      .then(response => response.json())
+      .then(data => setImages([...images,...data.results]))
+      .catch((err)=>alert(err));
+  }
+
+  useEffect(function () {
+    getImages();
+  }, [page]);
+
+  function handleButtonClick(e){
+    e.preventDefault();
+    console.log('zzz');
+    setImages([]);
+    setPage(1);
+  }
+
+  if (!accessKey) return (
+    <a href="https://unsplash.com/developers" className='error'>Create Your Unsplash Key</a>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Unsplash Image Gallery</h1>
+      <form onSubmit={handleButtonClick}>
+        <input type="text" onChange={(e)=>setQuery(e.target.value)} placeholder='Search Unsplash ...' />
+        <button>Search</button>
+      </form>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={()=>setPage(page+1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className='grid-body'>
+          {
+            images.map((image, index) => (
+              <div className='grid-item' key={index}>
+                <img src={image.urls.regular} alt={image.alt_description} />
+              </div>
+            ))
+          }
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
